318a551934a0ce6730f19079a80631cd6dd63db4
Bret Huisenga <huisenga@wisc.edu>
2016-05-17 15:39:58 -0500

    Removed Backup Files from root

--- BAK/BandCentral/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BandCentral.MvcApplication" Language="C#" %>
--- OLD/BandAngluar/Scripts/App/app.js
+++
@@ -1,7 +0,0 @@
-// Install the angularjs.TypeScript.DefinitelyTyped NuGet package 
-var App;
-(function (App) {
-    "use strict";
-    // Create the module and define its dependencies.
-    angular.module("app", []);
-})(App || (App = {}));
