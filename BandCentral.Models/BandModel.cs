d3ca938fb5f2985f9cfc417fff15dfaab54a2b8d
Bret Huisenga <huisenga@wisc.edu>
2016-05-17 19:42:00 -0500

    Include BandCentral.Model

---
+++ BandCentral.Models/BandModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandCentral.Model
+{
+    public class Band
+    {
+        public  int ID { get; set; }
+        public string BandName { get; set; }
+
+        public virtual List<ApplicationUser> User { get; set; }
+
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+
+        public Band()
+        {
+            DateCreated = DateTime.Now;
+        }
+    }
+}
